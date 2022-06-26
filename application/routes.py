6fa71d5d6377a33e4decee86220cf5b84c7b38f6
Alejandro <jaagomezmu@unal.edu.co>
2022-06-25 19:51:11 -0500

    graficas plotly con muestra de datos reales
    Se corrigió el layout para cada link, se añaden datos reales para configurar los botones y los componentes de filtrado. Se realizaron gráficas para establecer el mecanismo a usar con las siguientes figuras.

--- application/routes.py
+++ application/routes.py
@@ -5,14 +5,23 @@ import json
 import plotly
 import plotly.express as px
 
-@app.route("/index")
+transactions = pd.read_parquet(r'application\data\sample_transactions.parquet')
+users = pd.read_parquet(r'application\data\sample_users.parquet')
+
+
+@app.route("/")
 def index():
     return render_template("index.html", title = "Home")
 
 @app.route("/report")
 def report():
-    return render_template("report.html", title = "Reports")
+    fig = px.bar(transactions, x='fecha_transaccion', y='movilizado', color='producto')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("report.html", title = "Reports",graphJSON=graphJSON)
 
 @app.route("/user")
 def user():
-    return render_template("user.html", title = "Users")
+    
+    fig = px.histogram(users, x='genero')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("user.html", title = "Users",graphJSON=graphJSON)
